0c3509781355e0047be45d12d0e0fda3b004d04c
Zig Zichterman <github@ziggr.com>
2018-04-21 21:25:49 -0700

    Housing totals look good
    Data has prices for just about everything.

--- ZZHousingInventory.lua
+++ ZZHousingInventory.lua
@@ -38,7 +38,7 @@ end
 
 function Cost:ToStorage()
     local tm = ZZHousingInventory.ToMoney
-    local s = "%sg %dc %sv"
+    local s = "%sg %sc %sv"
     return string.format( s
                         , tm(self.gold     or 0)
                         , tm(self.crowns   or 0)
@@ -52,7 +52,7 @@ end
 
 
 local Item = {}
-function Item:FromFurnitureId(furniture_id)
+function Item:FromFurnitureId(furniture_id, count_collectibles)
     local o = { furniture_id = furniture_id }
 
     local r = { GetPlacedHousingFurnitureInfo(furniture_id) }
@@ -72,13 +72,127 @@ function Item:FromFurnitureId(furniture_id)
     o.furniture_subcategory_id  = r[2]
     o.furniture_theme_type      = r[3]
 
+                        -- De-attune before fetching price
+                        -- so that MM and FurC can supply
+                        -- pre-bound prices.
+    Item.Unattune(o)
+
     o.mm                 = ZZHousingInventory.MMPrice(o.link)
     o.furc               = ZZHousingInventory.FurCPrice(o.link)
+
+                        -- Supply Zig data for things we know
+                        -- but neither MM nor Furniture Catalog do.
+    Item.SupplyZigCost(o, count_collectibles)
 
     setmetatable(o, self)
     self.__index = self
     return o
 end
+
+function Item.Unattune(item)
+    if not item and item.name then return end
+
+                        -- Remove the "attuning" that makes these
+                        -- crafting stations unique, preventing FurC
+                        -- and MM from coming up with prices, AND
+                        -- preventing us from combining them into a single
+                        -- stack of count 41 instead of 41 unique items.
+                        --
+                        -- Note trailing space after each station name!
+                        -- Required to avoid false match on non-attunable
+                        -- stations.
+    if string.find(item.item_name, "Blacksmithing Station ") then
+        d("Un-attuning: "..item.item_name)
+        item.item_name = "Attunable Blacksmithing Station"
+        item.link = "|H1:item:119594:364:50:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:10000:0|h|h"
+        item.furniture_data_id = 4050
+    elseif string.find(item.item_name, "Clothing Station ") then
+        d("Un-attuning: "..item.item_name)
+        item.item_name = "Attunable Clothier Station"
+        item.link      = "|H1:item:119821:364:50:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:10000:0|h|h"
+        item.furniture_data_id = 4051
+    elseif string.find(item.item_name, "Woodworking Station ") then
+        d("Un-attuning: "..item.item_name)
+        item.item_name = "Attunable Woodworking Station"
+        item.link     = "|H1:item:119822:364:50:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:10000:0|h|h"
+        item.furniture_data_id = 4052
+    end
+end
+
+Item.ZIG_COST = {
+  ["The Apprentice"                         ] = Cost:New({ crowns = 250 })
+, ["The Atronach"                           ] = Cost:New({ crowns = 250 })
+, ["The Tower"                              ] = Cost:New({ crowns = 250 })
+, ["The Thief"                              ] = Cost:New({ crowns = 250 })
+, ["The Serpent"                            ] = Cost:New({ crowns = 250 })
+, ["The Ritual"                             ] = Cost:New({ crowns = 250 })
+, ["The Mage"                               ] = Cost:New({ crowns = 250 })
+, ["The Lady"                               ] = Cost:New({ crowns = 250 })
+, ["The Lord"                               ] = Cost:New({ crowns = 250 })
+, ["The Warrior"                            ] = Cost:New({ crowns = 250 })
+, ["The Lover"                              ] = Cost:New({ crowns = 250 })
+, ["The Steed"                              ] = Cost:New({ crowns = 250 })
+, ["The Shadow"                             ] = Cost:New({ crowns = 250 })
+
+, ["Transmute Station"                      ] = Cost:New({ crowns = 1250 })
+, ["Statue of Azura, Queen of Dawn and Dusk"] = Cost:New({ crowns = 4000 })
+, ["Hedge, Dense High Wall"                 ] = Cost:New({ crowns =   45 })
+, ["Topiary, Strong Cypress"                ] = Cost:New({ crowns =   45 })
+, ["Seal of Molag Bal"                      ] = Cost:New({ crowns = 5000 })
+}
+-- Things that appear in multiple houses. Count these collectibles only once,
+-- in the Grand Linchal Manor
+Item.ZIG_COST_COLLECTIBLE = {
+   --torage Coffer, Fortified"              ] = Cost:New({ crowns =  100 }) one of these was free for reaching level 18
+  ["Storage Coffer, Secure"                 ] = Cost:New({ crowns =  100 })
+, ["Storage Coffer, Sturdy"                 ] = Cost:New({ crowns =  100 })
+, ["Storage Coffer, Oaken"                  ] = Cost:New({ crowns =  100 })
+, ["Storage Chest, Fortified"               ] = Cost:New({ crowns =  200 })
+, ["Storage Chest, Oaken"                   ] = Cost:New({ crowns =  200 })
+, ["Storage Chest, Secure"                  ] = Cost:New({ crowns =  200 })
+, ["Storage Chest, Sturdy"                  ] = Cost:New({ crowns =  200 })
+
+, ["Nuzhimeh the Merchant"                  ] = Cost:New({ crowns = 5000 })
+--["Pirharri the Smuggler"                  ] = Cost:New() -- Free for completing Thieves Guild quest line
+, ["Tythis Andromo, the Banker"             ] = Cost:New({ crowns = 5000 })
+}
+
+function Item.SupplyZigCost(item, count_collectibles)
+
+                        -- Flag things without prices so that I can
+                        -- easily find them in SavedVariables
+    if not (o.mm or o.furc) then
+        o.missing_price = 1
+    end
+
+                        -- Supply costs for things we know but
+                        -- Furniture Catalogue does not.
+    if item.mm or item.furc then return end
+
+    local zc = Item.ZIG_COST[item.item_name]
+    if (not zc) and count_collectibles then
+        zc = Item.ZIG_COST_COLLECTIBLE[item.item_name]
+    end
+    if zc then
+        if 0 < zc.gold then
+            item.furc = { currency_type = "gold"
+                        , currency_ct   = zc.gold
+                        }
+        elseif 0 < zc.crowns then
+            item.furc = { currency_type = "crowns"
+                        , currency_ct   = zc.crowns
+                        }
+        elseif 0 < zc.vouchers then
+            item.furc = { currency_type = "vouchers"
+                        , currency_ct   = zc.vouchers
+                        }
+        end
+        if item.furc then
+            item.furc.notes = "Zig data"
+        end
+    end
+end
+
 
 function Item:Cost()
                         -- Lazy calc and cache.
@@ -117,6 +231,12 @@ function Item.ToStorage(self)
                   , value_mm   = ZZHousingInventory.Round(self.mm)
                   , value_furc = self.furc
                   }
+                        -- Flag items with no price data so that I can
+                        -- search for them in savedVariables later and
+                        -- supply a price if I want.
+    if not (store.value_mm or store.value_furc) then
+        store.no_cost = 1
+    end
     return key, store
 end
 
@@ -129,6 +249,7 @@ function ZZHousingInventory.ToMoney(x)
     if not x then return "?" end
     return ZO_CurrencyControl_FormatCurrency(ZZHousingInventory.Round(x), false)
 end
+
 
 -- Init ----------------------------------------------------------------------
 
@@ -269,6 +390,7 @@ function ZZHousingInventory:ScanNow()
         d("ZZHousing: not in a house. Exiting.")
         return
     end
+    local is_linchal = house_id == HOUSE.LINCHAL_MANOR.id
 
                         -- Find the HOUSE constant from above for this
                         -- current house.
@@ -286,7 +408,7 @@ function ZZHousingInventory:ScanNow()
     local furniture_id = GetNextPlacedHousingFurnitureId(nil)
     local loop_limit   = 1000 -- avoid infinite loops in case GNPHFI() surprises us
     while furniture_id and 0 < loop_limit do
-        local item = Item:FromFurnitureId(furniture_id)
+        local item = Item:FromFurnitureId(furniture_id, is_linchal)
         local key, store = item:ToStorage()
         save_furniture[key]    = save_furniture[key] or store
         save_furniture[key].ct = save_furniture[key].ct + 1
